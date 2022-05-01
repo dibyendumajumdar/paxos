dc3a3586f301d63692787570f1c0c10e7d8c89d6
dibyendumajumdar <mobile@majumdar.org.uk>
2022-05-01 22:36:51 +0100

    unit tests

---
+++ basic/src/test/java/org/redukti/paxos/basic/TestMessages.java
@@ -0,0 +1,78 @@
+package org.redukti.paxos.basic;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.redukti.paxos.log.api.BallotNum;
+import org.redukti.paxos.log.api.Decree;
+import org.redukti.paxos.net.impl.CorrelationId;
+
+import java.nio.ByteBuffer;
+
+public class TestMessages {
+
+    CorrelationId correlationId = new CorrelationId(1,101);
+    BallotNum b = new BallotNum(39, 3);
+    Decree d = new Decree(13, 94);
+    Vote v = new Vote(3, b, d);
+
+    @Test
+    public void testClientRequestMessage() {
+        ClientRequestMessage crm = new ClientRequestMessage(correlationId, 42);
+        ByteBuffer bb = crm.serialize();
+        ClientRequestMessage crm2 = (ClientRequestMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assert.assertEquals(crm.requestedValue, crm2.requestedValue);
+    }
+
+    @Test
+    public void testClientRessponeMessage() {
+        ClientResponseMessage crm = new ClientResponseMessage(42);
+        ByteBuffer bb = crm.serialize();
+        ClientResponseMessage crm2 = (ClientResponseMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assert.assertEquals(crm.agreedValue, crm2.agreedValue);
+    }
+
+    @Test
+    public void testNextBallotMessage() {
+        NextBallotMessage m = new NextBallotMessage(b);
+        ByteBuffer bb = m.serialize();
+        NextBallotMessage m2 = (NextBallotMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assert.assertEquals(m.b, m2.b);
+    }
+
+    @Test
+    public void testLastVoteMessage() {
+        LastVoteMessage m = new LastVoteMessage(b, v);
+        ByteBuffer bb = m.serialize();
+        LastVoteMessage m2 = (LastVoteMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assert.assertEquals(m.b, m2.b);
+        Assert.assertEquals(m.v, m2.v);
+        Assert.assertEquals(m.v.decree, m2.v.decree);
+    }
+
+    @Test
+    public void testBeginBallotMessage() {
+        BeginBallotMessage m = new BeginBallotMessage(b, d);
+        ByteBuffer bb = m.serialize();
+        BeginBallotMessage m2 = (BeginBallotMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assert.assertEquals(m.b, m2.b);
+        Assert.assertEquals(m.decree, m2.decree);
+    }
+
+    @Test
+    public void testVotedMessage() {
+        VotedMessage m = new VotedMessage(b, 3);
+        ByteBuffer bb = m.serialize();
+        VotedMessage m2 = (VotedMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assert.assertEquals(m.b, m2.b);
+        Assert.assertEquals(m.owner, m2.owner);
+    }
+
+    @Test
+    public void testSuccessMessage() {
+        SuccessMessage m = new SuccessMessage(d);
+        ByteBuffer bb = m.serialize();
+        SuccessMessage m2 = (SuccessMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assert.assertEquals(m.decree, m2.decree);
+    }
+
+}
