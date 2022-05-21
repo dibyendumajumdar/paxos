a837020682196e745e7e03ecef37ca10084e2a4b
dibyendumajumdar <mobile@majumdar.org.uk>
2022-05-21 20:16:49 +0100

    more tests

--- multi/src/test/java/org/redukti/paxos/multi/TestMessages.java
+++ multi/src/test/java/org/redukti/paxos/multi/TestMessages.java
@@ -13,6 +13,7 @@ public class TestMessages {
 
     CorrelationId correlationId = new CorrelationId(1,101);
     BallotNum b = new BallotNum(39, 3);
+    BallotNum maxBal = new BallotNum(45, 1);
     Decree d = new Decree(13, 94);
     Decree d2 = new Decree(14, 76);
     Vote v = new Vote(3, b, d);
@@ -26,13 +27,14 @@ public class TestMessages {
         Assertions.assertEquals(crm.requestedValue, crm2.requestedValue);
     }
 
-//    @Test
-//    public void testClientRessponeMessage() {
-//        ClientResponseMessage crm = new ClientResponseMessage(42);
-//        ByteBuffer bb = crm.serialize();
-//        ClientResponseMessage crm2 = (ClientResponseMessage) PaxosMessages.parseMessage(correlationId, bb);
-//        Assertions.assertEquals(crm.agreedValue, crm2.agreedValue);
-//    }
+    @Test
+    public void testClientResponseMessage() {
+        ClientResponseMessage crm = new ClientResponseMessage(1, 42);
+        ByteBuffer bb = crm.serialize();
+        ClientResponseMessage crm2 = (ClientResponseMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assertions.assertEquals(crm.agreedValue, crm2.agreedValue);
+        Assertions.assertEquals(crm.dnum, crm2.dnum);
+    }
 
     @Test
     public void testNextBallotMessage() {
@@ -48,7 +50,6 @@ public class TestMessages {
 
     @Test
     public void testLastVoteMessage() {
-
         LastVoteMessage m = new LastVoteMessage(b, 3, 101, new Vote[]{v, v2});
         ByteBuffer bb = m.serialize();
         LastVoteMessage m2 = (LastVoteMessage) PaxosMessages.parseMessage(correlationId, bb);
@@ -71,18 +72,31 @@ public class TestMessages {
         Assertions.assertEquals(101, m.cnum);
         Assertions.assertEquals(2, m.chosenDecrees.length);
         Assertions.assertTrue(Arrays.equals(m.chosenDecrees, m2.chosenDecrees));
+        Assertions.assertEquals(0, m.committedDecrees.length);
+        Assertions.assertEquals(m.pid, m2.pid);
+        Assertions.assertEquals(m.cnum, m2.cnum);
+
+        m = new BeginBallotMessage(b, 3, 101, new Decree[0], new Decree[]{d, d2});
+        bb = m.serialize();
+        m2 = (BeginBallotMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assertions.assertEquals(m.b, m2.b);
+        Assertions.assertEquals(3, m.pid);
+        Assertions.assertEquals(101, m.cnum);
+        Assertions.assertEquals(0, m.chosenDecrees.length);
+        Assertions.assertEquals(2, m.committedDecrees.length);
+        Assertions.assertTrue(Arrays.equals(m.committedDecrees, m2.committedDecrees));
         Assertions.assertEquals(m.pid, m2.pid);
         Assertions.assertEquals(m.cnum, m2.cnum);
     }
-//
-//    @Test
-//    public void testVotedMessage() {
-//        VotedMessage m = new VotedMessage(b, 3);
-//        ByteBuffer bb = m.serialize();
-//        VotedMessage m2 = (VotedMessage) PaxosMessages.parseMessage(correlationId, bb);
-//        Assertions.assertEquals(m.b, m2.b);
-//        Assertions.assertEquals(m.owner, m2.owner);
-//    }
+
+    @Test
+    public void testVotedMessage() {
+        VotedMessage m = new VotedMessage(b, 3);
+        ByteBuffer bb = m.serialize();
+        VotedMessage m2 = (VotedMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assertions.assertEquals(m.b, m2.b);
+        Assertions.assertEquals(m.pid, m2.pid);
+    }
 
     @Test
     public void testSuccessMessage() {
@@ -95,4 +109,13 @@ public class TestMessages {
         Assertions.assertEquals(m.decree[1], m2.decree[1]);
     }
 
+    @Test
+    public void testNackMessage() {
+        NackMessage m = new NackMessage(b, maxBal, 1);
+        ByteBuffer bb = m.serialize();
+        NackMessage m2 = (NackMessage) PaxosMessages.parseMessage(correlationId, bb);
+        Assertions.assertEquals(m.b, m2.b);
+        Assertions.assertEquals(m.maxBal, m2.maxBal);
+        Assertions.assertEquals(m.pid, m2.pid);
+    }
 }
